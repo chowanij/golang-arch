a7cec8fd3d0c548fbaf544274ce8766677c477ba
janchowaniec <janchowaniec@odrabiamy.pl>
2022-08-04 22:03:53 +0200

    hash password for save and compare using bcrypt

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type person struct {
@@ -37,11 +39,28 @@ func main() {
 		log.Panic(err)
 	}
 
+	pass := "123456789"
+
+	hssPass, err := hashPassword(pass)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = comparePassword(pass, hssPass)
+
+	if err != nil {
+		log.Fatalln("Not logged in")
+	}
+
+	log.Println("Loged in")
+
 	fmt.Println("back into a Go data structure", xp2)
 
 	http.HandleFunc("/encode", foo)
 	http.HandleFunc("/decode", bar)
 	http.ListenAndServe(":8080", nil)
+	
 
 }
 
@@ -69,4 +88,20 @@ func bar(w http.ResponseWriter, r *http.Request) {
 func base64Encode() {
 	fmt.Println("encode string to 64 demo")
 	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error while generating bcrypt hash from password: %w", err)
+	}
+	return bs, nil
+}
+
+func comparePassword(password string, hashedPass []byte) error{
+	err := bcrypt.CompareHashAndPassword(hashedPass, []byte (password))
+	if err != nil {
+		return fmt.Errorf("Invalid password: %w", err)
+	}
+	return nil
 }
